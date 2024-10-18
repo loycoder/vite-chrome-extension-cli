4c94fbd855d46608d3b9c5125c36676a76bf9d26
liangyong <liangyong9098@cvte.com>
2024-10-18 11:49:37 +0800

    feat: 模拟 cookie 注入的逻辑

---
+++ content-script/src/service/background.ts
@@ -0,0 +1,34 @@
+// src/background.ts
+
+chrome.runtime.onInstalled.addListener(() => {
+ // 打开新窗口
+ // chrome.tabs.create({
+ //  url: 'https://baidu.com/1'
+ // })
+});
+
+chrome.cookies.getAll({ domain: 'id.seewo.com' }, (cookies) => {
+ console.log('cookies: ', cookies);
+})
+
+const cookie = {
+ url: "https://baidu.com",
+ name: "your_cookie_name",
+ value: "your_cookie_value",
+ expirationDate: (new Date().getTime() / 1000) + 3600 // 1 hour from now
+};
+
+chrome.cookies.set(cookie, (cookie) => {
+ if (chrome.runtime.lastError) {
+   console.error(chrome.runtime.lastError);
+ } else {
+   console.log("Cookie set: ", cookie);
+ }
+});
+
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+ if (changeInfo.status === 'complete' && tab.url) {
+   console.log(`Tab updated: ${tab.url}`);
+ }
+});
